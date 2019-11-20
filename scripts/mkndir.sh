23465fdc897cd0a29a1726b45d61ba53b923b739
ktor-vi <v.hm.philippe@gmail.com>
2019-11-20 16:53:35 +0100

    Added 2 scripts

---
+++ scripts/mkndir.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+echo "Enter directory name"
+read newdir
+`mkdir $newdir`
---
+++ scripts/userinput.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+echo "What is your name ?"
+read name
+echo "Thank you for attending my workshop, $name"!
